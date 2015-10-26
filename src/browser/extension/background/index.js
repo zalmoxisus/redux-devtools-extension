036be0e163f046895d435e3e326813e48aaf8770
Zalmoxisus <zalmoxisus@gmail.com>
2015-10-26 21:40:01 +0200

    Inject content script from manifest

--- src/browser/extension/background/index.js
+++ src/browser/extension/background/index.js
@@ -1,10 +1,7 @@
 import configureStore from '../../../app/store/configureStore';
 import createMenu from './contextMenus';
 
-configureStore(store => {
-  createMenu(store);
-  
-  require('./inject');
 
-  window.store = store;
-}, true);
+createMenu(store);
+
+window.store = store;
