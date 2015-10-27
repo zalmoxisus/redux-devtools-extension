eb6e3208645daea9408aeb84d4408f498e75f4ef
Zalmoxisus <zalmoxisus@gmail.com>
2015-10-27 10:03:52 +0200

    Communication with the injected script

--- src/browser/extension/inject/contentScript.js
+++ src/browser/extension/inject/contentScript.js
@@ -1,7 +1,51 @@
-console.log('Content script injected!');
-var s = document.createElement('script');
+import { ACTION, UPDATE } from '../../../app/constants/ActionTypes';
+
+let s = document.createElement('script');
 s.src = chrome.extension.getURL('js/page.bundle.js');
 s.onload = function() {
   this.parentNode.removeChild(this);
 };
 (document.head || document.documentElement).appendChild(s);
+
+// Communicate with the devtool
+window.addEventListener('message', function(event) {
+  if (event && event.source !== window) {
+    return;
+  }
+
+  const message = event.data;
+
+  if (typeof message !== 'object' || message === null || message.source !== 'redux-page') {
+    return;
+  }
+
+  chrome.runtime.sendMessage(message);
+});
+
+// Send actions to the page
+window.dispatch = function(action) {
+  window.postMessage({
+    type: ACTION,
+    payload: action,
+    source: 'redux-cs'
+  }, '*')
+};
+
+// Ask for updates from the page
+window.update = function() {
+  window.postMessage({
+    type: UPDATE,
+    source: 'redux-cs'
+  }, '*')
+};
+
+// Request from the background script to send actions to the page
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action) window.dispatch(message.action);
+});
+
+window.addEventListener('beforeunload', function() {
+  chrome.runtime.sendMessage({
+    type: 'PAGE_UNLOADED'
+  })
+});
--- src/browser/extension/inject/pageScript.js
+++ src/browser/extension/inject/pageScript.js
@@ -1,3 +1,47 @@
-window.sayHi = function() {
-  alert('hi')
+import { createStore, applyMiddleware, compose } from 'redux';
+import DevTools from '../../../app/containers/DevTools';
+import { ACTION, UPDATE } from '../../../app/constants/ActionTypes';
+
+window.devToolsInit = function(store) {
+  function onChange() {
+    window.postMessage({
+      payload: store.liftedStore.getState(),
+      source: 'redux-page'
+    }, '*')
+  }
+
+  function onMessage(event) {
+    let message;
+
+    if (event && event.source !== window) {
+      return
+    }
+
+    message = event.data;
+
+    if (!message || message.source !== 'redux-cs') {
+      return;
+    }
+
+    if (message.type === ACTION) {
+      store.liftedStore.dispatch(message.payload);
+    }
+
+    if (message.type === UPDATE) {
+      onChange();
+    }
+  }
+
+  store.liftedStore.subscribe(onChange);
+  window.addEventListener('message', onMessage);
+
+  onChange();
+};
+
+window.devToolsExtension = function(next) {
+  return (reducer, initialState) => {
+    const store = DevTools.instrument()(next)(reducer, initialState);
+    devToolsInit(store);
+    return store;
+  }
 };
