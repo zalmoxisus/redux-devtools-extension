0285c9f542eff0cb632b1574a7d15f2f5065b11f
Zalmoxisus <zalmoxisus@gmail.com>
2015-10-28 14:30:17 +0200

    Nuke ./build from github

--- build/extension/js/devtools.bundle.js
+++
@@ -1,1 +0,0 @@
-!function(e){function t(r){if(n[r])return n[r].exports;var o=n[r]={exports:{},id:r,loaded:!1};return e[r].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}({0:function(e,t,n){e.exports=n(454)},454:function(e,t){"use strict";chrome.devtools.panels.create("Redux",null,"devpanel.html",function(e){})}});
