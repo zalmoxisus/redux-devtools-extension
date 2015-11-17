f7b32a1321fad5200bf44ce956fe02a4a7793af8
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-17 18:26:54 +0200

    Configure pages urls to inject DevTools in
    Fix #3.

--- src/browser/extension/options/getOptions.js
+++ src/browser/extension/options/getOptions.js
@@ -1,7 +1,9 @@
 const getOptions = callback => {
   chrome.storage.sync.get({
     timeout: 1,
-    serialize: true
+    serialize: true,
+    inject: true,
+    urls: 'https?:\\/\\/localhost|0\\.0\\.0\\.0:\\d+\\/(.+)?\nhttps?:\\/\\/.+\\.github\\.io\\/(.+)?'
   }, function(items) {
     callback(items)
   });
--- src/browser/extension/options/index.js
+++ src/browser/extension/options/index.js
@@ -6,7 +6,27 @@ const saveOption = e => {
   let obj = {};
   obj[e.target.id] = e.target.type === 'checkbox' ? e.target.checked : Number(e.target.value);
   chrome.storage.sync.set(obj);
-}
+};
+
+const isValidRegex = str => {
+  let isValid = true;
+  try {
+    new RegExp(str);
+  } catch (e) {
+    isValid = false;
+  }
+  return isValid;
+};
+
+const saveUrls = e => {
+  let obj = {};
+  const urls = e.target.value;
+  urls.split('\n').forEach(function(url) {
+    if (!isValidRegex(url)) return; // TODO: show an error message
+  });
+  obj[e.target.id] = urls;
+  chrome.storage.sync.set(obj);
+};
 
 getOptions( items => {
   render(
@@ -20,6 +40,15 @@ getOptions( items => {
         <span className="caption">States serialization:</span>
         <input id="serialize" type="checkbox" defaultChecked={items.serialize} onChange={saveOption} />
         <span className="comment">(required for circular references)</span>
+      </div>
+      <div className="input">
+        <span className="caption">Inject in all pages:</span>
+        <input id="inject" type="checkbox" defaultChecked={items.inject} onChange={saveOption} />
+        <span className="comment">(disable to allow only the urls bellow)</span>
+      </div>
+      <div className="input">
+        <span className="caption">Pages urls to inject DevTools in (regex from new line):</span>
+        <textarea onChange={saveUrls} id="urls" defaultValue={items.urls} />
       </div>
     </div>,
     document.getElementById('root')
