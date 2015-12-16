80b836fe8b09ddebb9e03fdf06297e07ef34674d
Zalmoxisus <zalmoxisus@gmail.com>
2015-12-16 11:52:49 +0200

    Use 'redux-devtools-log-monitor-filtrable'
    https://github.com/zalmoxisus/redux-devtools-log-monitor-filterable

--- src/app/containers/DevTools.js
+++ src/app/containers/DevTools.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
+import LogMonitor from 'redux-devtools-log-monitor-filtrable';
 
 export default createDevTools(<LogMonitor />);
