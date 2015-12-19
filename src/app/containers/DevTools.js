4b707dc943849f9262a7138c5f65ef80a165dcce
Zalmoxisus <zalmoxisus@gmail.com>
2015-12-19 14:04:21 +0200

    Fix a dependency

--- src/app/containers/DevTools.js
+++ src/app/containers/DevTools.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor-filtrable';
+import LogMonitor from 'redux-devtools-log-monitor-filterable';
 
 export default createDevTools(<LogMonitor />);
