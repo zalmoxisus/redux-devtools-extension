5cf95c0ac028d128ba490fe5c1f3d08151488e6d
Zalmoxisus <zalmoxisus@gmail.com>
2015-12-24 18:00:57 +0200

    Prevent error flooding

--- src/browser/extension/utils/notifyErrors.js
+++ src/browser/extension/utils/notifyErrors.js
@@ -1,4 +1,22 @@
 let handleError;
+let lastTime = 0;
+
+function createExpBackoffTimer(step) {
+  let count = 1;
+  return function(reset) {
+    // Reset call
+    if (reset) {
+      count = 1;
+      return 0;
+    }
+    // Calculate next timeout
+    let timeout = Math.pow(2, count - 1);
+    count += 1;
+    return timeout * step;
+  };
+}
+
+const nextErrorTimeout = createExpBackoffTimer(1000);
 
 function postError(message) {
   if (handleError && !handleError()) return;
@@ -10,7 +28,11 @@ function postError(message) {
 }
 
 function catchErrors(e) {
-  if (window.devToolsOptions && !window.devToolsOptions.notifyErrors) return;
+  if (
+    window.devToolsOptions && !window.devToolsOptions.notifyErrors
+    || e.timeStamp - lastTime < nextErrorTimeout()
+  ) return;
+  lastTime = e.timeStamp; nextErrorTimeout(true);
   postError(e.message);
 }
 
