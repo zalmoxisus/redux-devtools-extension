c4a544d9ace3730867618993ffdf95b4c5b80d45
Zalmoxisus <zalmoxisus@gmail.com>
2016-04-12 12:14:19 +0300

    Remove `redux-devtools-filter-actions`

--- src/app/containers/Monitor.js
+++ src/app/containers/Monitor.js
@@ -1,5 +1,4 @@
-import React, { createElement } from 'react';
-import FilterMonitor from 'redux-devtools-filter-actions';
+import { createElement } from 'react';
 import LogMonitor from 'redux-devtools-log-monitor';
 import ChartMonitor from 'redux-devtools-chart-monitor';
 import SliderMonitor from 'redux-slider-monitor';
@@ -8,16 +7,12 @@ import InspectorMonitor from 'redux-devtools-inspector';
 
 const monitorTypes = location.hash ? location.hash.substr(1).split('/') : {};
 
-function getMonitor() {
+export default (props) => {
   switch (monitorTypes[0]) {
-    case 'SliderMonitor': return createElement(SliderMonitor);
-    case 'ChartMonitor': return createElement(ChartMonitor);
-    case 'DiffMonitor': return createElement(DiffMonitor);
-    case 'InspectorMonitor': return createElement(InspectorMonitor);
-    default: return createElement(LogMonitor, { preserveScrollTop: false });
+    case 'SliderMonitor': return createElement(SliderMonitor, props);
+    case 'ChartMonitor': return createElement(ChartMonitor, props);
+    case 'DiffMonitor': return createElement(DiffMonitor, props);
+    case 'InspectorMonitor': return createElement(InspectorMonitor, props);
+    default: return createElement(LogMonitor, { ...props, preserveScrollTop: false });
   }
-}
-
-export default (props) => (
-  <FilterMonitor {...props}>{getMonitor()}</FilterMonitor>
-);
+};
