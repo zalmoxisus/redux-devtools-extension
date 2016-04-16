e1146fede7db668b30fd05657030286da0d9cb83
Zalmoxisus <zalmoxisus@gmail.com>
2016-04-16 15:25:48 +0300

    Add styles for ChartMonitor

--- src/app/containers/Monitor.js
+++ src/app/containers/Monitor.js
@@ -10,7 +10,17 @@ const monitorTypes = location.hash ? location.hash.substr(1).split('/') : {};
 export default (props) => {
   switch (monitorTypes[0]) {
     case 'SliderMonitor': return createElement(SliderMonitor, props);
-    case 'ChartMonitor': return createElement(ChartMonitor, { ...props, defaultIsVisible: true });
+    case 'ChartMonitor': return createElement(ChartMonitor, {
+      ...props, defaultIsVisible: true, invertTheme: true,
+      tooltipOptions: {
+        style: {
+          'background-color': '#ffffff',
+          'opacity': '0.9',
+          'border-radius': '5px',
+          'padding': '5px'
+        }
+      }
+    });
     case 'DiffMonitor': return createElement(DiffMonitor, props);
     case 'InspectorMonitor': return createElement(InspectorMonitor, props);
     default: return createElement(LogMonitor, { ...props, preserveScrollTop: false });
