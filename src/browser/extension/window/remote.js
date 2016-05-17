b2d3336b8dd1ceebf3da0180744c10c86206e0b2
Zalmoxisus <zalmoxisus@gmail.com>
2016-05-17 17:15:05 +0300

    Persist remotedev-app options

--- src/browser/extension/window/remote.js
+++ src/browser/extension/window/remote.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import DevTools from 'remotedev-app';
 
-render(
-  <DevTools/>,
-  document.getElementById('root')
-);
+chrome.storage.local.get({
+  'select-monitor': 'LogMonitor',
+  's:hostname': null,
+  's:port': null,
+  's:secure': null
+}, options => {
+  render(
+    <DevTools
+      selectMonitor={options['select-monitor']}
+      socketOptions={
+        options['s:hostname'] && options['s:port'] ?
+          {
+            hostname: options['s:hostname'], port: options['s:port'], secure: options['s:secure']
+          } : undefined
+      }
+    />,
+    document.getElementById('root')
+  );
+});
