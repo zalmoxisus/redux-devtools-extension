6a538fe59f8d32ac1ecb7f8ba8901088625b24a6
Zalmoxisus <zalmoxisus@gmail.com>
2016-05-27 13:49:45 +0300

    Send changes to  react-counter-messaging

--- examples/react-counter-messaging/components/Counter.js
+++ examples/react-counter-messaging/components/Counter.js
@@ -1,5 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 
+const withDevTools = (
+  // process.env.NODE_ENV === 'development' &&
+  typeof window !== 'undefined' && window.devToolsExtension
+);
+
 class Counter extends Component {
   constructor() {
     super();
@@ -9,13 +14,27 @@ class Counter extends Component {
     this.decrement = this.decrement.bind(this);
   }
 
+  componentWillMount() {
+    if (withDevTools) {
+      this.devTools = window.devToolsExtension.connect();
+    }
+  }
+
+  componentWillUnmount() {
+    if (withDevTools) {
+      window.devToolsExtension.disconnect();
+    }
+  }
+
   increment() {
     const state = { counter: this.state.counter + 1 };
+    if (withDevTools) this.devTools.send('increment', state);
     this.setState(state);
   }
 
   decrement() {
     const state = { counter: this.state.counter - 1 };
+    if (withDevTools) this.devTools.send('decrement', state);
     this.setState(state);
   }
 
