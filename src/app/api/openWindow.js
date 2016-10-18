13d1fc4a3b684863b4bcc4bf28b1d6b68a30a3b0
Zalmoxisus <zalmoxisus@gmail.com>
2016-10-18 18:07:38 +0300

    Warn when opening the extension's window explicitely
    Related to #229.

--- src/app/api/openWindow.js
+++ src/app/api/openWindow.js
@@ -1,4 +1,9 @@
 export default function openWindow(position) {
+  /* eslint-disable no-console */
+  console.warn('Redux DevTools Extension\'s window was opened as ' +
+    '`window.devToolsExtension.open()` was called from the app. ' +
+    'Remove this command for better experience.');
+  /* eslint-enable no-console */
   window.postMessage({
     source: '@devtools-page',
     type: 'OPEN',
