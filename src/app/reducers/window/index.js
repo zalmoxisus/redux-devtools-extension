15161035f0c97d1a5ad9621bc599a845a2510a26
Zalmoxisus <zalmoxisus@gmail.com>
2016-11-10 12:51:43 +0200

    Support for remote reports

--- src/app/reducers/window/index.js
+++ src/app/reducers/window/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import instances from './instances';
 import monitor from 'remotedev-app/lib/reducers/monitor';
 import notification from 'remotedev-app/lib/reducers/notification';
+import socket from 'remotedev-app/lib/reducers/socket';
 import reports from 'remotedev-app/lib/reducers/reports';
 import test from 'remotedev-app/lib/reducers/test';
 
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
   instances,
   monitor,
   test,
+  socket,
   reports,
   notification
 });
--- src/app/stores/windowStore.js
+++ src/app/stores/windowStore.js
@@ -1,12 +1,14 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import persist from 'remotedev-app/lib/middlewares/persist';
+import api from 'remotedev-app/lib/middlewares/api';
+import { CONNECT_REQUEST } from 'remotedev-app/lib/constants/socketActionTypes';
 import syncStores from '../middlewares/windowSync';
 import popupSelector from '../middlewares/popupSelector';
 import rootReducer from '../reducers/window';
 
 export default function configureStore(baseStore, position, preloadedState) {
   let enhancer;
-  const middlewares = [syncStores(baseStore), persist(position)];
+  const middlewares = [api, syncStores(baseStore), persist(position)];
   if (position === '#popup') middlewares.push(popupSelector);
   if (process.env.NODE_ENV === 'production') {
     enhancer = applyMiddleware(...middlewares);
@@ -17,6 +19,14 @@ export default function configureStore(baseStore, position, preloadedState) {
     );
   }
   const store = createStore(rootReducer, preloadedState, enhancer);
+
+  chrome.storage.local.get(['s:hostname', 's:port', 's:secure'], options => {
+    if (!options['s:hostname'] || !options['s:port']) return;
+    store.dispatch({
+      type: CONNECT_REQUEST,
+      options: { hostname: options['s:hostname'], port: options['s:port'], secure: options['s:secure'] }
+    });
+  });
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
