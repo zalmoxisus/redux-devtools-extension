f418596a3a00d0a6bef3a6be258540bf7bf169a0
Zalmoxisus <zalmoxisus@gmail.com>
2017-01-31 13:45:39 +0200

    Implement `logOnly` module
    Related to #289.

---
+++ npm-package/logOnly.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var assign = require('./utils/assign');
+var compose = require('redux').compose;
+
+function enhancer() {
+  var config = arguments[0] || {};
+  return function(createStore) {
+    return function(reducer, preloadedState, enhancer) {
+      var store = createStore(reducer, preloadedState, enhancer);
+      var origDispatch = store.dispatch;
+
+      var devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect(config);
+      devTools.init(store.getState());
+
+      var dispatch = function(action) {
+        var r = origDispatch(action);
+        devTools.send(action, store.getState());
+        return r;
+      };
+
+      if (Object.assign) return Object.assign(store, { dispatch: dispatch });
+      return assign(store, 'dispatch', dispatch);
+    }
+  }
+}
+
+function composeWithEnhancer(config) {
+  return function () {
+    return compose(compose.apply(null, arguments), enhancer(config));
+  }
+}
+
+exports.__esModule = true;
+exports.composeWithDevTools = function() {
+  if (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__) {
+    if (arguments.length === 0) return enhancer();
+    if (typeof arguments[0] === 'object') return composeWithEnhancer(arguments[0]);
+    return composeWithEnhancer().apply(null, arguments);
+  }
+
+  if (arguments.length === 0) return undefined;
+  if (typeof arguments[0] === 'object') return compose;
+  return compose.apply(null, arguments);
+};
+
