ecea4e28783592ff22fab0b0fedb9cdb854d5614
Zalmoxisus <zalmoxisus@gmail.com>
2017-02-05 12:11:35 +0200

    Remove warning for `openWindow`

--- src/app/api/openWindow.js
+++ src/app/api/openWindow.js
@@ -1,9 +1,4 @@
 export default function openWindow(position) {
-  /* eslint-disable no-console */
-  console.warn('Redux DevTools Extension\'s window was opened as ' +
-    '`window.devToolsExtension.open()` was called. ' +
-    'Remove this command from your code for better experience.');
-  /* eslint-enable no-console */
   window.postMessage({
     source: '@devtools-page',
     type: 'OPEN',
