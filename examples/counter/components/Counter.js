9604ece9ce3318eb8bac9c1f1cdbe8944991a599
thanawat <tha_chookes@hotmail.com>
2017-05-02 18:28:52 +0700

    update to react 15.5 and add prop-types

--- examples/counter/components/Counter.js
+++ examples/counter/components/Counter.js
@@ -1,5 +1,7 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { isMonitorAction } from '../store/configureStore';
+
 
 class Counter extends Component {
   render() {
