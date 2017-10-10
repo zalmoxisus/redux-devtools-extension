339386a68da6d631db7d018a71f843042c68df27
Fernando Jorge Mota <f.j.mota13@gmail.com>
2017-10-10 13:33:14 -0300

    Add browser field and browser versions of the main libraries

---
+++ npm-package/index.browser.js
@@ -0,0 +1,23 @@
+"use strict";
+
+import { compose } from "redux";
+
+const composeWithDevTools = (
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :
+    function() {
+      if (arguments.length === 0) return undefined;
+      if (typeof arguments[0] === 'object') return compose;
+      return compose.apply(null, arguments);
+    }
+);
+
+export composeWithDevTools;
+
+const devToolsEnhancer = (
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION__ :
+    function() { return function(noop) { return noop; } }
+);
+
+export devToolsEnhancer;
