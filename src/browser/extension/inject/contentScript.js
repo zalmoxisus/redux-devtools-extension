6276d0ea83faaea8f4aff421a3e5c479e2f18d41
Zalmoxisus <zalmoxisus@gmail.com>
2018-11-25 00:26:22 +0200

    Workaround for new Chrome limit on message size

--- src/browser/extension/inject/contentScript.js
+++ src/browser/extension/inject/contentScript.js
@@ -1,5 +1,7 @@
 import { injectOptions, isAllowed } from '../options/syncOptions';
 const source = '@devtools-extension';
+const pageSource = '@devtools-page';
+const maxChromeMsgSize = 32 * 1024 * 1024 - 1048576; // 32 - 1 MB (1 MB reserved for other object's parts)
 let connected = false;
 let bg;
 
@@ -48,6 +50,36 @@ function tryCatch(fn, args) {
   try {
     return fn(args);
   } catch (err) {
+    if (err.message === 'Message length exceeded maximum allowed length.') {
+      const instanceId = args.instanceId;
+      const newArgs = { split: 'start' };
+      const toSplit = [];
+      let size = 0;
+      let arg;
+      Object.keys(args).map(key => {
+        arg = args[key];
+        if (typeof arg === 'string') {
+          size += arg.length;
+          if (size > maxChromeMsgSize) {
+            toSplit.push([key, arg]);
+            return;
+          }
+        }
+        newArgs[key] = arg;
+      });
+      fn(newArgs);
+      for (let i = 0; i < toSplit.length; i++) {
+        for (let j = 0; j < toSplit[i][1].length; j += maxChromeMsgSize) {
+          fn({
+            instanceId,
+            source: pageSource,
+            split: 'chunk',
+            chunk: [toSplit[i][0], toSplit[i][1].substr(j, maxChromeMsgSize)]
+          });
+        }
+      }
+      return fn({ instanceId, source: pageSource, split: 'end' });
+    }
     handleDisconnect();
     /* eslint-disable no-console */
     if (process.env.NODE_ENV !== 'production') console.error('Failed to send message', err);
@@ -69,7 +101,7 @@ function handleMessages(event) {
   if (!isAllowed()) return;
   if (!event || event.source !== window || typeof event.data !== 'object') return;
   const message = event.data;
-  if (message.source !== '@devtools-page') return;
+  if (message.source !== pageSource) return;
   if (message.type === 'DISCONNECT') {
     if (bg) {
       bg.disconnect();
