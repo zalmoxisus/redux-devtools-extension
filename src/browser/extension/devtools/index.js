d11eb65a9ad31a25f89d206f48ef442b4e8d7bd3
Zalmoxisus <zalmoxisus@gmail.com>
2018-11-29 01:01:40 +0200

    Workaround for devpanel not having access in incognito mode
    Related to #597 and 580

--- src/browser/extension/devtools/index.js
+++ src/browser/extension/devtools/index.js
@@ -1,3 +1,14 @@
-chrome.devtools.panels.create(
-  'Redux', 'img/logo/scalable.png', chrome.extension.getBackgroundPage ? 'window.html' : 'devpanel.html', function() {}
-);
+function createPanel(url) {
+  chrome.devtools.panels.create(
+    'Redux', 'img/logo/scalable.png', url, function() {}
+  );
+}
+
+if (chrome.runtime.getBackgroundPage) {
+  // Check if the background page's object is accessible (not in incognito)
+  chrome.runtime.getBackgroundPage(background => {
+    createPanel(background ? 'window.html' : 'devpanel.html');
+  });
+} else {
+  createPanel('devpanel.html');
+}
