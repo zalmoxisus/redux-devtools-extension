44a73aa1b7a14ff611c0b1f30387e027efec2188
Anastasia <inoteol@gmail.com>
2019-01-05 16:54:30 +0200

    Update examples (#621)

--- examples/counter/webpack.config.js
+++ examples/counter/webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   devtool: 'source-map',
   entry: [
     'webpack-hot-middleware/client',
@@ -13,16 +14,13 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
-      loaders: ['babel'],
-      exclude: /node_modules/,
-      include: __dirname
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
     }]
   }
 };
--- examples/todomvc/store/configureStore.js
+++ examples/todomvc/store/configureStore.js
@@ -15,8 +15,7 @@ export default function configureStore(preloadedState) {
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers');
-      store.replaceReducer(nextReducer);
+      store.replaceReducer(require('../reducers').default)
     });
   }
 
