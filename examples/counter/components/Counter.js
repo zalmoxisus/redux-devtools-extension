156a90a16572fcbded48b3e99b29157c6397bc19
deanban <pbp2005@gmail.com>
2019-07-24 18:05:33 -0400

    component is unnecessarily class based

--- examples/counter/components/Counter.js
+++ examples/counter/components/Counter.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Counter extends Component {
-  render() {
-    const { increment, autoIncrement, incrementAsync, decrement, counter } = this.props;
-    return (
-      <p>
-        Clicked: {counter} times
-        {' '}
-        <button onClick={increment}>+</button>
-        {' '}
-        <button onClick={decrement}>-</button>
-        {' '}
-        <button onClick={incrementAsync}>Increment async</button>
-        {' '}
-        <button onClick={autoIncrement}>Auto increment</button>
-      </p>
-    );
-  }
-}
+const Counter = props => {
+  const {
+    increment,
+    autoIncrement,
+    incrementAsync,
+    decrement,
+    counter
+  } = props;
+  return (
+    <p>
+      Clicked: {counter} times <button onClick={increment}>+</button>{' '}
+      <button onClick={decrement}>-</button>{' '}
+      <button onClick={incrementAsync}>Increment async</button>{' '}
+      <button onClick={autoIncrement}>Auto increment</button>
+    </p>
+  );
+};
 
 Counter.propTypes = {
   increment: PropTypes.func.isRequired,
